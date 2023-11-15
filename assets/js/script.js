239b938d460ad80a8bd5b8374c431075a02ef7be
Ahmed <ahmedkhalidsh98@gmail.com>
2023-11-15 12:45:28 +0100

    test 4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     fetchEquipmentData();
+    initializeFeatureModals();
 });
 
 function fetchEquipmentData() {
@@ -53,6 +54,99 @@ function displayEquipment(equipmentArray) {
         // Append the card to the equipment list
         equipmentList.appendChild(card);
     });
+}
+
+function initializeFeatureModals() {
+    const featuresInfo = {
+        'always-open': {
+            title: 'Always Open',
+            description: 'We are open 24/7 to fit your busy schedule.'
+        },
+        'equipment-training': {
+            title: 'Equipment Training',
+            description: 'Our gym is equipped with a wide variety of machines to cover all your needs.'
+        },
+        'cardio-training': {
+            title: 'Cardio Training',
+            description: 'State-of-the-art treadmills, bikes, and ellipticals await you.'
+        },
+        'free-weight-area': {
+            title: 'Free Weight Area',
+            description: 'Our free weights range allows for a complete strength workout.'
+        },
+        'functional-training': {
+            title: 'Functional Training',
+            description: 'Functional training area for exercises that translate into real-world activities.'
+        },
+        'trx-training': {
+            title: 'TRX Training',
+            description: 'Suspension training to develop strength, balance, and flexibility.'
+        },
+        'stretching-point': {
+            title: 'Stretching Point',
+            description: 'Dedicated area for stretching and improving your flexibility.'
+        },
+        'box-workout': {
+            title: 'Box Workout',
+            description: 'Get your heart pumping with our boxing workouts.'
+        },
+        'personal-training': {
+            title: 'Personal Training',
+            description: 'One-on-one sessions with certified trainers to meet your goals.'
+        },
+        'inbody-analysis': {
+            title: 'InBody Analysis',
+            description: 'Precise body composition analysis to track your progress.'
+        },
+        'solarium': {
+            title: 'Solarium',
+            description: 'Enjoy our solarium facilities for a healthy glow.'
+        },
+        'refreshments': {
+            title: 'Refreshments',
+            description: 'Stay hydrated and energized with our selection of drinks.'
+        },
+        'showers': {
+            title: 'Showers',
+            description: 'Clean and well-maintained showers for your comfort.'
+        },
+        'training-plan': {
+            title: 'Training Plan',
+            description: 'Custom training plans tailored to your fitness level.'
+        },
+        'wifi': {
+            title: 'WiFi',
+            description: 'Free WiFi across the gym so you can stay connected.'
+        },
+        'parking': {
+            title: 'Parking',
+            description: 'Ample parking space for all our gym visitors.'
+        }
+    };
+
+    document.querySelectorAll('.gym-picture').forEach(function(element) {
+        element.addEventListener('click', function() {
+            const featureKey = this.getAttribute('data-feature');
+            const featureData = featuresInfo[featureKey];
+            
+            if (featureData) {
+                updateModalContent(featureData.title, featureData.description);
+            } else {
+                console.error('Feature data not found for:', featureKey);
+            }
+        });
+    });
+}
+
+function updateModalContent(title, description) {
+    const modalTitle = document.getElementById('infoModalLabel');
+    const modalBody = document.querySelector('#infoModal .modal-body');
+
+    modalTitle.textContent = title;
+    modalBody.textContent = description;
+
+    var infoModal = new bootstrap.Modal(document.getElementById('infoModal'));
+    infoModal.show();
 }
 
 // Use jQuery to handle the modal related events
